5ff5762d8c9c7490cb297cd272e10324e5ce3028
Joshua Nathaniel Miller <jmiller@hackcodeboot.com>
2022-02-19 09:57:30 -0600

    Update dependencies

--- jest.config.js
+++ jest.config.js
@@ -40,8 +40,10 @@ module.exports = {
   ],
 
   // An object that configures minimum threshold enforcement for coverage results
-  coverageThreshold: {
+  "coverageThreshold": {
     "global": {
+      "branches": 100,
+      "functions": 100,
       "lines": 100,
       "statements": 100
     }
